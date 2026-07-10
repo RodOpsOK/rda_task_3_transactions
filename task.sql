58747622ed7d03e02a6505609af8966a753c510a
Oleh K <hmkl.public@gmail.com>
2026-07-10 12:50:14 +0200

    upd: added order_count

--- task.sql
+++ task.sql
@@ -5,16 +5,17 @@ USE ShopDB;
 INSERT INTO Orders (CustomerID, Date) 
 VALUES (1, '2023-01-01');
 
+SET @order_count = 1;
+
 -- Start the transaction 
 START TRANSACTION; 
 
--- And some data should be created inside the transaction 
-
+-- And some data should be created inside the transaction
 INSERT INTO OrderItems (OrderID, ProductID, Count) 
-VALUES (LAST_INSERT_ID(), 1, 1);
+VALUES (LAST_INSERT_ID(), 1, @order_count);
 
 UPDATE Products 
-SET WarehouseAmount = WarehouseAmount - 1 
+SET WarehouseAmount = WarehouseAmount - @order_count 
 WHERE ID = 1;
 
 COMMIT;
